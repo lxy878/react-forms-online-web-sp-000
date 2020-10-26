36a124a8eb6e47ab8a57284153b0cbb3252d3ae7
Xiaoyi Liu <lxy878@gmail.com>
2020-10-26 13:56:30 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -26,6 +26,12 @@ class Form extends React.Component {
     this.setState({submittedData: dataArray})
   }
 
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+      return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+    })
+  }
+
   render() {
     return (
       <div>
